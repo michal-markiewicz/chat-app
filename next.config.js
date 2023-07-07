59fb474ab87446971742eca4c6aa355c14d896d1
Michal Markiewicz <michalmarkiewicz0953@gmail.com>
2023-07-07 14:59:38 +0200

    Remove everything related to socket io

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil",
-    });
+const nextConfig = {}
 
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
