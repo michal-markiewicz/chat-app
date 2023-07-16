c392b89c7d018d582443ce18dbb8f98800d752ca
Michal Markiewicz <michalmarkiewicz0953@gmail.com>
2023-07-16 09:47:54 +0200

    Add sql fixtures to userService tests

--- src/app/server/tests/UserService.test.ts
+++ src/app/server/tests/UserService.test.ts
@@ -1,37 +1,52 @@
+import mysql2 from "mysql2/promise";
+import DatabaseService from "../helpers/DatabaseService";
 import UserService from "../helpers/UserService";
 
+const databaseService = new DatabaseService();
 const userService = new UserService();
 
-const user = {
-  username: "test",
-  password: "123456",
-};
+let connection: mysql2.Connection;
+
+beforeAll(async () => {
+  connection = await databaseService.connect();
+  await connection.query(`
+  INSERT INTO user (id, username, password) VALUES ('1', 'insertExisting', '123456');
+  `);
+  await connection.query(
+    `  INSERT INTO user (id, username, password) VALUES ('2', 'get', '123456');`
+  );
+  await connection.query(
+    `  INSERT INTO user (id, username, password) VALUES ('3', 'delete', '123456');`
+  );
+});
 
 describe("UserService", () => {
   describe("register", () => {
     test("should save user in a database", async () => {
       const user = {
-        username: "registration_test",
+        username: "insert",
         password: "123456",
       };
+
       await userService.register(user);
       const result = await userService.getUser({ username: user.username });
-
       expect(result.username).toBe(user.username);
-      await userService.deleteUser({ username: user.username });
     });
     test("should return error when user already exists", async () => {
+      const user = {
+        username: "insertExisting",
+        password: "123456",
+      };
+
       await userService.register(user);
       const result = await userService.register(user);
-
       expect(result.errorMsg).toBe("User with this username already exists.");
-      await userService.deleteUser({ username: user.username });
     });
   });
 
   describe("getUser", () => {
     test("should get user by username", async () => {
-      const username = "get_test";
+      const username = "get";
       const result = await userService.getUser({ username });
       expect(result.username).toBe(username);
     });
@@ -39,11 +54,15 @@ describe("UserService", () => {
 
   describe("deleteUser", () => {
     test("should delete user", async () => {
-      await userService.register(user);
-      await userService.deleteUser({ username: user.username });
-      const result = await userService.getUser({ username: user.username });
-
+      const username = "delete";
+      await userService.deleteUser({ username });
+      const result = await userService.getUser({ username });
       expect(result?.username).toBeFalsy();
     });
   });
+});
+
+afterAll(async () => {
+  await connection.query(`DELETE FROM user`);
+  await connection.end();
 });
