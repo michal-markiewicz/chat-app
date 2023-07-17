6da9d385ededdab13e0db52be0c02ce84fff4a11
Michal Markiewicz <michalmarkiewicz0953@gmail.com>
2023-07-17 10:40:04 +0200

    Improve saveMessage security

--- src/app/server/helpers/ChatService.ts
+++ src/app/server/helpers/ChatService.ts
@@ -1,10 +1,10 @@
 import { Message } from "@/app/components/chatMessage/ChatMessage";
 import {
-    IChatService,
-    IFetchMessagesResult,
-    ISaveMessageRequest,
-    ISaveMessageResult,
+  IChatService,
+  ISaveMessageRequest,
+  ISaveMessageResult,
 } from "@/app/shared/contracts/IChatService";
+import dayjs from "dayjs";
 import { RowDataPacket } from "mysql2";
 import DatabaseService from "./DatabaseService";
 import "./server-only";
@@ -16,27 +16,43 @@ export default class ChatService implements IChatService {
   }
   async saveMessage(request: ISaveMessageRequest): Promise<ISaveMessageResult> {
     const { sender, content, date } = request;
+
+    if (!sender || !content || !date) {
+      return {
+        errorMsg: "Missing data",
+      };
+    }
+
+    if (
+      typeof sender !== "string" ||
+      typeof content !== "string" ||
+      !dayjs(date).isValid()
+    ) {
+      return {
+        errorMsg: "Invalid data",
+      };
+    }
+
     const dbConnection = await this.databaseService.connect();
 
     await dbConnection.execute(
       "INSERT INTO messages (sender, content, date) VALUES (?, ?, ?)",
-      [sender, content, date]
+      [sender, content, new Date(date)]
     );
 
     await dbConnection.end();
     return { errorMsg: "" };
   }
 
-  async fetchMessages(): Promise<IFetchMessagesResult> {
+  async fetchMessages(): Promise<Message[]> {
     const dbConnection = await this.databaseService.connect();
     let rows: RowDataPacket[] = [];
 
-      const [result] = await dbConnection.execute<RowDataPacket[]>(
-        "SELECT * FROM messages"
-      );
-      rows.push(...result);
-      await dbConnection.end();
-        return rows as Message[];
-    }
+    const [result] = await dbConnection.execute<RowDataPacket[]>(
+      "SELECT * FROM messages"
+    );
+    rows.push(...result);
+    await dbConnection.end();
+    return rows as Message[];
   }
 }
